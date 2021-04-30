b23ffef303f13b6ce60ccebbcce116104d603b7e
djsaunders1997@gmail.com <djsaunders1997@gmail.com>
2021-04-30 10:58:20 +0000

    Added messages to master only notebook

---
+++ Master-Only-Notebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+print('This is a master only notebook')
+
+# COMMAND ----------
+
+print('This is a master only notebook')
+
+# COMMAND ----------
+
+print('This is a master only notebook')
+
+# COMMAND ----------
+
+print('This is a master only notebook')
+
+# COMMAND ----------
+
+print('This is a master only notebook')
